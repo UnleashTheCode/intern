76c9b30457c9c8275e051624d1715365d7cf9852
RootLikeGroot <andreighita18@gmail.com>
2020-11-26 22:44:15 +0200

    teest1

--- login.php
+++ login.php
@@ -42,8 +42,8 @@ require 'auth_control.php';
         border-style: solid;
         border-width: 5px;
         border-radius: 15px;
-        border-image-slice: 1;
-        border-image-source: linear-gradient(to right, #373b44, #4286f4);
+        /* border-image-slice: 1; */
+        border-image: linear-gradient(to right, #373b44, #4286f4);
         
     }
 
