bd02d99f9036489343058686d573e22065f3d996
RootLikeGroot <andreighita18@gmail.com>
2020-11-26 21:52:07 +0200

    test1

--- login.php
+++ login.php
@@ -10,11 +10,12 @@ require 'auth_control.php';
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 
 <style>
-    * {
+    /* * {
         margin: 0;
         padding: 0;
-    }
-    html {
+    } */
+
+    html, body {
         text-align: center;
         background-image: url("./upload/santa.jpg");
         background-repeat: no-repeat;
@@ -22,13 +23,7 @@ require 'auth_control.php';
         max-width: 100%;
         height: 100%;
     }
-    /* body {
-        text-align: center;
-        background-image: url("./upload/santa.jpg");
-        background-repeat: no-repeat;
-        background-size: cover;
-        max-width: 100%;
-    } */
+
     form {
         display: inline-block;
     }
